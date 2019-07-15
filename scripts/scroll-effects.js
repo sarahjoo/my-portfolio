dc8f0fb376fc1799c7a62941e0c5d0982d25f4d9
sahjoo <joosangah@gmail.com>
2019-07-15 01:39:02 -0400

    fixed shifting navbar

--- scripts/scroll-effects.js
+++ scripts/scroll-effects.js
@@ -1,18 +1,18 @@
 // When the user scrolls the page, execute stickIt
 window.onscroll = function() {stickIt()};
 
-// Get the header
-var header = document.getElementById("stickyMenu");
+// Get the navbar
+var navbar = document.getElementById("stickyMenu");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = navbar.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickIt() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
   } else {
-    header.classList.remove("sticky");
+    navbar.classList.remove("sticky");
   }
 }
 
